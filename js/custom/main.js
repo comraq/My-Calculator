92c3e5f21410d14e15caf84f1792f1fb05abef90
comraq <yinadam@hotmail.com>
2015-12-11 16:55:13 -0800

    finished implementing the previously diabled buttons

--- js/custom/main.js
+++ js/custom/main.js
@@ -17,11 +17,7 @@ function buttonClick() {
         if ($userEntry.val().length == 0) {
           warn(val);
         } else {
-          if (exprStack[exprStack.length - 1] == "-") {
-            exprStack.splice(-1, 1, "+", $userEntry.val()*(-1));
-          } else {
-            exprStack.push($userEntry.val());
-          }
+          stackCurrentNum($userEntry.val());
           updateResult(eval(exprStack).toString());
         }
         break;
@@ -29,18 +25,39 @@ function buttonClick() {
       case "-":
       case "*":
       case "/":
+      case "%":
         if ($userEntry.val().length == 0) {
           warn(val);
         } else { 
-          if (exprStack[exprStack.length - 1] == "-") {
-            exprStack.splice(-1, 1, "+", $userEntry.val()*(-1));
-          } else {
-            exprStack.push($userEntry.val()); //Pushes numbers before the current operator
-          }
+          stackCurrentNum($userEntry.val());
           if (val == "+" || val == "-") checkPrecedence();
           exprStack.push(val); //Pushes the current operator
           updateResult(exprStack.toString().replace(/,/g, ""));
         }
+        break;
+      case "^":
+        if ($userEntry.val().length == 0) {
+          warn("pow");
+        } else {
+          var exp = prompt("Enter nth power for base " + $userEntry.val() + ":", "2");
+          if (isNaN(exp)) {
+            warn("pow");
+          } else {
+            $userEntry.val(Math.pow($userEntry.val(), exp));
+          }; 
+        }; 
+        break;
+      case "r":
+        if ($userEntry.val().length == 0) {
+          warn("val");
+        } else {
+          var root = prompt("Enter nth root for number " + $userEntry.val() + ":", "2");
+          if (parseInt(root) == "NaN" || parseInt(root) != root || root < 2) {
+            warn("root");
+          } else {
+            $userEntry.val(Math.pow($userEntry.val(), 1/root));
+          };
+        };
         break;
       case "s":
         if (parseFloat($userEntry.val()) == "NaN") {
@@ -49,11 +66,70 @@ function buttonClick() {
           $userEntry.val(parseFloat($userEntry.val())*(-1));
         }    
         break;
-      case "d":
+      case "n":
         $userEntry.val($userEntry.val().substring(0, $userEntry.val().length - 1));
         break;
       case "l":
-        updateResult($userEntry.val());
+        if ($userEntry.val().length == 0) {
+          warn("lg");
+        } else {
+          $userEntry.val(Math.log($userEntry.val(), 2));
+        }
+        break;
+      case "p":
+        if ($userEntry.val().length == 0) {
+          warn("2^x");
+        } else {
+          $userEntry.val(Math.pow(2,$userEntry.val()));
+        };
+        break;
+      case "b":
+        var res = $("#result").val();
+        if (res == "") {
+          if (isNaN($userEntry.val())) {
+            warn("to Binary");
+          } else {
+            updateResult((parseFloat($userEntry.val()) >>> 0).toString(2));
+          }
+        } else if (!isNaN(parseFloat(res, 10))) {
+          updateResult((parseFloat(res) >>> 0).toString(2));
+        } else if (!isNaN(parseFloat(res, 16))) {
+          updateResult((parseFloat(res) >>> 0).toString(2));
+        } else {
+          warn("to Binary");
+        }; 
+        break;    
+      case "d":
+        var res = $("#result").val();
+        if (res == "") {
+          if (isNaN($userEntry.val())) {
+            warn("to Decimal");
+          } else {
+            updateResult(parseInt($userEntry.val()).toString());
+          }
+        } else if (!isNaN(parseInt(res, 2))) {
+          updateResult(parseInt(res, 2).toString(10));
+        } else if (!isNaN(parseInt(res, 16))) {
+          updateResult(parseInt(res, 16).toString(10));
+        } else {
+          warn("to Decimal");
+        };
+        break;
+      case "h":
+        var res = $("#result").val();
+        if (res == "") {
+          if (isNaN($userEntry.val())) {
+            warn("to Hexaecimal");
+          } else {
+            updateResult(parseInt($userEntry.val()).toString(16).toUpperCase());
+          }
+        } else if (!isNaN(parseInt(res, 2))) {
+          updateResult(parseInt(res, 2).toString(16).toUpperCase());
+        } else if (!isNaN(parseInt(re))) {
+          updateResult(parseInt(res).toString(16).toUpperCase());
+        } else {
+          warn("to Decimal");
+        };
         break;
       default: 
         alert("Case not yet implemented");
@@ -61,13 +137,23 @@ function buttonClick() {
   }
 };
 
+function stackCurrentNum(num) {
+  if (exprStack[exprStack.length - 1] == "-") {
+    exprStack.splice(-1, 1, "+", num*(-1));
+  } else {
+    exprStack.push(num); //Pushes numbers before the current operator
+  }
+};
+
 function checkPrecedence() {
   if (exprStack.length < 3) return;
 
-  var op = exprStack[exprStack.length - 2];
-  if (op == "*" || op == "/" || op == "%") {
-    exprStack.splice(-3, 3, eval(exprStack.slice(-3)));
-    checkPrecedence();
+  if (exprStack[exprStack.length - 1][0] != "(") {
+    var op = exprStack[exprStack.length - 2];
+    if (op == "*" || op == "/" || op == "%") {
+      exprStack.splice(-3, 3, eval(exprStack.slice(-3)));
+      checkPrecedence();
+    };
   };
 }
 
@@ -102,6 +188,13 @@ function eval(expression) {
   };
 };
 
+Math.log = (function() {
+  var log = Math.log;
+  return function(n, base) {
+    return log(n)/((base)? log(base) : 1);
+  };
+})();
+
 function updateResult(res) {
   if (typeof(res) == "undefined") {
     alert("Invalid Expression!");
@@ -117,6 +210,7 @@ function warn(op) {
 
 function clear() {
   exprStack = [];
+  bracket = 0;
   updateResult("");
 };
 
