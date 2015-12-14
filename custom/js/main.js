eecf844e2ed0274c9e03640f2f2d35e628da5a78
comraq <yinadam@hotmail.com>
2015-12-13 21:26:34 -0800

    updates to footer

--- custom/js/main.js
+++ custom/js/main.js
@@ -1,19 +1,63 @@
-$(document).ready(function() {
-  $("#title").mouseenter(function() {
-    $("#title").fadeTo('slow', 1);
+var main = function() {
+  $(".content-pane").fadeTo(600, 1);
+
+  $(".nav a").on("click", function() {
+    navBar.navUpdateActive(this);
+    contentPane.updateContent(this);
   });
+ 
+  $(".project").on({mouseenter: projects.markActive,
+                    mouseleave: projects.markInactive});
+};
 
-  $("#title").mouseleave(function() {
-    $("#title").fadeTo('slow', 0.3);
-  }); 
+var navBar = {
+  navUpdateActive: function(navComp) {
+    $(".nav").find(".my-navbar-active").removeClass("my-navbar-active");
+    if (!$(navComp).hasClass("dropdown-toggle")) {
+      $(navComp).parent().addClass("my-navbar-active");
+    };
+  }
+};
 
-  $("button").click(function() {
-    $(".wip-description").show();
-  }); 
+var contentPane = {
+  updateContent: function(navComp) {
+    contentPane.removeContent(navComp);
+    contentPane.addContent(navComp);
+  },
 
-  $(".wip").mouseleave(function() {
-    $(".wip-description").hide();
-  });
+  removeContent: function(navComp) {
+    if (!$(navComp).hasClass("dropdown-toggle")) $(".my-containers").fadeTo(500, 0);
+  },
 
-  $(".wip-description").hide();
-});
+  addContent: function(navComp) {
+    if ($(navComp).is("#nav-home")) {
+      $(".home-container").fadeTo(500, 1);
+    } else if ($(navComp).is("#nav-about")) {
+      $(".about-container").fadeTo(500, 1);
+    } else if ($(navComp).is("#nav-projects")) {
+      $(".projects-container").fadeTo(500, 1);
+    };
+  }
+};
+
+var projects = {
+  markInactive: function() {
+    if ($(this).hasClass("project-jumbo")) {
+      $(this).removeClass("rainbow");
+      $(this).children("h1").removeClass("rainbow");
+    } else {
+      $(this).fadeTo(500, 0.7);
+    }
+  },
+
+  markActive: function() {
+    if ($(this).hasClass("project-jumbo")) {
+      $(this).addClass("rainbow");
+      $(this).children("h1").addClass("rainbow");
+    } else {
+      $(this).fadeTo(500, 1);
+    }
+  }
+}
+
+$(document).ready(main);
